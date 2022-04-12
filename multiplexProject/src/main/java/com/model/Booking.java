a93de34bee6f1bad55417cd421a57ce1a94e841c
Mani-2210 <101783074+Mani-2210@users.noreply.github.com>
2022-04-12 09:29:30 +0530

    Add files via upload

---
+++ multiplexProject/src/main/java/com/model/Booking.java
@@ -0,0 +1,49 @@
+package com.model;
+
+import java.sql.Date;
+
+import javax.persistence.*;
+
+@Entity
+public class Booking {
+	@Id
+    private int bookingId;
+	private int showId;
+	private int userId;
+	private Date bookDate;
+	private Date showDate;
+	public Booking() {}
+	public int getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+	public int getShowId() {
+		return showId;
+	}
+	public void setShowId(int showId) {
+		this.showId = showId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public Date getBookDate() {
+		return bookDate;
+	}
+	public void setBookDate(Date bookDate) {
+		this.bookDate = bookDate;
+	}
+	public Date getShowDate() {
+		return showDate;
+	}
+	public void setShowDate(Date showDate) {
+		this.showDate = showDate;
+	}
+	
+	
+
+}
